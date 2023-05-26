3a85709a2bb1a7b32841eadc833fd6ad591e5369
Jack Rabe <jhrabe@wisc.edu>
2023-05-25 21:57:46 -0500

    redo create user form look

--- frontend/src/routes/form.svelte
+++ frontend/src/routes/form.svelte
@@ -3,9 +3,6 @@
 	let form: HTMLFormElement;
 	function handleSubmit(_e: Event) {
 		const formData = new FormData(form);
-		if (!formData.get('location')) {
-			return;
-		}
 
 		fetch('http://localhost:4000/api/users', {
 			method: 'POST',
@@ -21,29 +18,32 @@
 </script>
 
 <form bind:this={form} on:submit|preventDefault={handleSubmit} class="form-control m-4 gap-4">
-	<label class="grid grid-cols-4">
-		Name:
-		<input name="name" type="text" required class="input-bordered input col-span-3" />
+	<label class="input-group w-1/2">
+		<span>Name</span>
+		<input name="name" type="text" required class="input-bordered input w-full" />
+	</label>
+	<label class="input-group w-1/2">
+		<span>Age</span>
+		<input name="age" type="number" required class="input-bordered input w-full" />
+	</label>
+	<label class="input-group w-1/2">
+		<span>Major</span>
+		<input name="major" type="text" required class="input-bordered input w-full" />
 	</label>
 
-	<label class="grid grid-cols-4">
-		Age:
-		<input name="age" type="number" required class="input-bordered input col-span-3" />
-	</label>
-
-	<label class="grid grid-cols-4">
-		Location:
-		<select name="location" required class="select-bordered select col-span-3">
-			<option selected disabled>Select Location</option>
+	<label class="input-group w-1/2">
+		<span>Location</span>
+		<!-- TODO fix this width -->
+		<select name="location" required class="select-bordered select w-full">
+			<option selected disabled />
 			{#each locationOptions as opt}
 				<option>{opt}</option>
 			{/each}
 		</select>
 	</label>
-
-	<label class="grid grid-cols-4">
-		Major:
-		<input name="major" type="string" required class="input-bordered input col-span-3" />
+	<label class="input-group">
+		<span>Profile Picture</span>
+		<input type="file" accept="image/*" class="file-input-bordered file-input" />
 	</label>
 
 	<button class="btn" type="submit">Submit</button>
